1d7aee2cc5465590f668abb71b4121a233b03167
CharlieDevs <krlosreyes2@gmail.com>
2026-04-17 22:58:49 -0500

    Spec 10 Terminado

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.metamorfosis.elena.elena_app"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -36,7 +36,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
--- android/app/src/main/kotlin/com/metamorfosis/elena/elena_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/metamorfosis/elena/elena_app/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.metamorfosis.elena.elena_app
 
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
-
-FirebaseApp.initializeApp(this)
-FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
-    PlayIntegrityAppCheckProviderFactory.getInstance()
-)
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance()
+        )
+    }
+}
